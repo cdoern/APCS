4c2e39a78a2821ffb7ba2a0970fa981b84e3116a
Charlie Doern <cbdoern@icloud.com>
2018-09-25 18:11:03 -0400

    added v2 which prints 2 max and min temps as well as the whole array

--- src/CharlieDoernWeatherAnalysis.java
+++ src/CharlieDoernWeatherAnalysis.java
@@ -1,5 +1,6 @@
 //This program asks a user to select an amount of days, input temperatures of those days and calculates the avg emp and the amount of days over that avg and prints the results.
 import java.util.*; //import scanner
+
 public class CharlieDoernWeatherAnalysis {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
@@ -38,5 +39,26 @@ public class CharlieDoernWeatherAnalysis {
             j++; //increment loop
         }
         System.out.print(over + " days were above average."); //print results
+        System.out.println();
+        System.out.println("Temperatures: "+ Arrays.toString(temps));
+        int max = Integer.MIN_VALUE;
+        int max2 = 0;
+        for(int i = 0; i<temps.length;i++){
+            if(temps[i]>max){
+                max2 = max;
+                max = temps[i];
+            }
+        }
+           int min = Integer.MAX_VALUE;
+        int min2 = 0;
+        for(int i = 0; i<temps.length;i++){
+            if(temps[i]<min){
+                min2 = min;
+                min = temps[i];
+            }
+        }
+        System.out.println("Two coldest days: " + min + ", " + min2);
+        System.out.print("Two hottest days: "+ max +", " + max2);
     }
+
 }
