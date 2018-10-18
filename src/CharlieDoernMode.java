2ce1a61bfb19254b0bd390bde659deb02d1ffb1d
cdoern <cbdoern@icloud.com>
2018-10-18 13:28:24 -0400

    array practice edited

---
+++ src/CharlieDoernMode.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class CharlieDoernMode {
+    public static void main(String[] args){
+        System.out.print(liamPriceAlgo(669260267));
+    }
+    public static int liamPriceAlgo(int num){
+        int length = String.valueOf(num).length();
+        int[] array = new int[length+1];
+        for(int i =length; i >0; i--){
+            array[num%10]+=1;
+            num= num/10;
+
+        }
+        int max = 0;
+        int in = 0;
+        for(int i = 0; i< array.length; i++){
+            if(array[i] == array[in]){
+                max =in;
+            }
+            if(array[i] > array[in]){
+                max = i;
+                in = i;
+            }
+
+        }
+        return(max);
+    }
+
+
+}
