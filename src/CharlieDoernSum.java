dc03790d7f8fd5a21c9ab45176c8d63ce7149ec3
cdoern <cbdoern@icloud.com>
2018-10-31 17:33:09 -0400

    edited sum

--- src/CharlieDoernSum.java
+++ src/CharlieDoernSum.java
@@ -1,14 +1,18 @@
-import java.io.File;
+//import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 public class CharlieDoernSum {
 
 public static void main(String[] args){
-    Scanner in = new Scanner(new File("sum.txt"));
+    Scanner in = new Scanner(System.in);
+    //Scanner in = new Scanner(new File("sum.txt"));
+
     add(in);
 }
 public static void add(Scanner in){
    // char[] let;
-    int[] num;
+    int[] num = new int[25];
+
     while(in.hasNextLine()){
     String line = in.nextLine();
     Scanner token = new Scanner(line);
@@ -18,13 +22,14 @@ public static void add(Scanner in){
         for(int i =0; i<let.length; i++){
             String s = "" +let[i];
            int nums = Integer.parseInt(s);
-
+            num[num.length-i-1] = nums;
 
         }
+
     }
     }
 
 
     }
 }
-}
+
